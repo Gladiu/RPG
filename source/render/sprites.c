387956c989dc35beb29b98c86bec95e9393f2b7d
Gladiu <maurycyk1@tlen.pl>
2021-09-23 22:52:02 +0000

    Cleaned up and finished rendering of placeholder sprite

--- source/render/sprites.c
+++ source/render/sprites.c
@@ -38,84 +38,45 @@ void InitSprites(sprites* inputSprite)
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (GLvoid*)(0));
 	glEnableVertexAttribArray(0);
 
-	// Opening files, if successfull progress further
-	FILE *vertexFile;
-	FILE *fragmentFile;
-	vertexFile = fopen("generic.vert", "r");
-	fragmentFile = fopen("generic.frag", "r");
+	// Creating Shaders
 
-	if (0)//vertexFile == NULL || fragmentFile == NULL)
+	GLint succes;
+	GLchar infoLog[512];
+	GLuint fragment;
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &genericFragmentShader, NULL);
+
+	glCompileShader(fragment);
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &succes);
+	if(!succes)
 	{
-		if (vertexFile == NULL)
-			fprintf(stderr, "FATAL ERROR. COULDNT OPEN VERTEX SHADER CODE\n");
-		else
-			fprintf(stderr, "FATAL ERROR. COULDNT OPEN FRAGMENT SHADER CODE\n");
+		glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+		fprintf(stderr, "FATAL. COULDNT COMPILE FRAGMENT SHADER %s\n", infoLog);
 	}
-	else
-	{
-		if(0)
-		{
-			// Reading fragment shader code
-			fseek(fragmentFile, 0, SEEK_END);
-			long length = ftell(fragmentFile);
-			fseek(fragmentFile, 0, SEEK_SET);
-			char * fragmentSource;
-			fragmentSource = malloc(length);
-			fread(fragmentSource, 1, length, fragmentFile );
 
-			// Reading vertex shader code
-			fseek(vertexFile, 0, SEEK_END);
-			length = ftell(vertexFile);
-			fseek(vertexFile, 0, SEEK_SET);
-			char * vertexSource;
-			vertexSource = malloc(length);
-			fread(vertexSource, 1, length, vertexFile );
-		}
+	GLuint vertex;
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &genericVertexShader, NULL);
+	glCompileShader(vertex);
+	glGetShaderiv(vertex, GL_COMPILE_STATUS, &succes);
+	if(!succes)
+	{
+		glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+		fprintf(stderr, "FATAL. COULDNT COMPILE VERTEX SHADER %s\n", infoLog);
+	}
 	
-
-		GLint succes;
-		GLchar infoLog[512];
-		// Creating shaders
-		GLuint fragment;
-		fragment = glCreateShader(GL_FRAGMENT_SHADER);
-		//glShaderSource(fragment, 1, &fragmentSource, NULL);
-		glShaderSource(fragment, 1, &genericFragmentShader, NULL);
-
-		glCompileShader(fragment);
-		glGetShaderiv(fragment, GL_COMPILE_STATUS, &succes);
-		if(!succes)
-		{
-			glGetShaderInfoLog(fragment, 512, NULL, infoLog);
-			fprintf(stderr, "FATAL. COULDNT COMPILE FRAGMENT SHADER %s\n", infoLog);
-		}
-
-		GLuint vertex;
-		vertex = glCreateShader(GL_VERTEX_SHADER);
-		//glShaderSource(vertex, 1, &vertexSource, NULL);
-		glShaderSource(vertex, 1, &genericVertexShader, NULL);
-		glCompileShader(vertex);
-		glGetShaderiv(vertex, GL_COMPILE_STATUS, &succes);
-		if(!succes)
-		{
-			glGetShaderInfoLog(vertex, 512, NULL, infoLog);
-			fprintf(stderr, "FATAL. COULDNT COMPILE VERTEX SHADER %s\n", infoLog);
-		}
-		
-		inputSprite->shaderProgram = glCreateProgram();
-		glAttachShader(inputSprite->shaderProgram, vertex);
-		glAttachShader(inputSprite->shaderProgram, fragment);
-		glLinkProgram(inputSprite->shaderProgram);
-		glGetProgramiv(inputSprite->shaderProgram, GL_LINK_STATUS, &succes);
-		if(!succes)
-		{
-			glGetProgramInfoLog(inputSprite->shaderProgram, 512, NULL, infoLog);
-			fprintf(stderr, "FATAL. COULDNT LINK SHADER PROGRAM %s\n", infoLog);
-		}
-		glDeleteShader(vertex);
-		glDeleteShader(fragment);
-		//fclose(vertexFile);
-		//fclose(fragmentFile);
+	inputSprite->shaderProgram = glCreateProgram();
+	glAttachShader(inputSprite->shaderProgram, vertex);
+	glAttachShader(inputSprite->shaderProgram, fragment);
+	glLinkProgram(inputSprite->shaderProgram);
+	glGetProgramiv(inputSprite->shaderProgram, GL_LINK_STATUS, &succes);
+	if(!succes)
+	{
+		glGetProgramInfoLog(inputSprite->shaderProgram, 512, NULL, infoLog);
+		fprintf(stderr, "FATAL. COULDNT LINK SHADER PROGRAM %s\n", infoLog);
 	}
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
 
 
 

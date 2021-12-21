afc7bc0bfd7d0bd5c6301f3e5bedfff827a14c0c
gladiu <maurcyk1@tlen.pl>
2021-12-21 23:58:06 +0100

    Added init function

--- source/logic/shape.c
+++ source/logic/shape.c
@@ -1,2 +1,13 @@
 #include "shape.h"
 
+void InitShape(shape inputShape, vec2 position, float angle, int pointCount, vec2 pointsArray[3]){
+
+	inputShape.position[0] = position[0];
+	inputShape.position[1] = position[1];
+	inputShape.pointCount = pointCount;
+	inputShape.pointsArray = calloc(1, sizeof(vec2)*pointCount);
+	for(int i = 0; i<pointCount; i++){
+		inputShape.pointsArray[i][0] = pointsArray[i][0];
+		inputShape.pointsArray[i][1] = pointsArray[i][1];
+	}
+}
--- source/logic/shape.h
+++ source/logic/shape.h
@@ -1,46 +1,22 @@
 #pragma once
 #include "../libs/cglm/call.h"
 #include <stdbool.h>
+#include <stdlib.h>
 
 // Used for collision check and physics
-// Might later implement other polygons than square
-// Below you can see graphical representation on how 
-// that looks in 2D space. I find this way most intuitive for this application
-// not necessairly the best for everything and might change
-// 
-//  Position point
-//  |
-//  |                         angle rotation direction
-//  |                             ----
-//  |                                ||
-//  |                                ||
-//  |                                \/
-// \ /     <-width->
-//  .______________________.
-//  |                      |
-//  |                      | /\
-//  |                      | h
-//  |                      | e
-//  |                      | i
-//  |                      | g
-//  |                      | h
-//  |                      | t
-//  |                      | \/
-//  |                      |
-//  .______________________.
-//
-//
+
 typedef struct shape
 {
-	float x;
-	float y;
-	float angle; 
-	float width;
-	float height;
+	vec2 position;
+	int pointCount;
+	// All points are relative to position vector
+	vec2 *pointsArray;
 }shape;
 
-void Move(shape* inputShape, vec2 desiredDirection);
+void InitShape(shape inputShape, vec2 position, float angle, int pointCount, vec2 pointsArray[3]);
 
-void Rotate(shape* inputShape, float angle);
+void SetPosShape(shape* inputShape, vec2 desiredPosition);
+
+void SetAngleShape(shape* inputShape, float angle);
 
 bool Collides(shape* shape1, shape shape2);

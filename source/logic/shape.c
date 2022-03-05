3ef4a3227d8b00d827c10ac51a109bf87a37e246
Gladiu <maurycyk1@tlen.pl>
2022-03-05 01:44:18 +0100

    Started implementing SAT for collision detection

--- source/logic/shape.c
+++ source/logic/shape.c
@@ -1,15 +1,18 @@
 #include "shape.h"
 #include "../libs/cglm/vec2.h"
+#include <math.h>
 
-void InitShape(shape inputShape, vec2 position, float angle, int pointCount, vec2 pointsArray[3]){
+void InitShape(shape* inputShape, vec2 position, float angle, int sidesCount, line* sides[3]){
 
-	inputShape.position[0] = position[0];
-	inputShape.position[1] = position[1];
-	inputShape.pointCount = pointCount;
-	inputShape.pointsArray = calloc(1, sizeof(vec2)*pointCount);
-	for(int i = 0; i<pointCount; i++){
-		inputShape.pointsArray[i][0] = pointsArray[i][0];
-		inputShape.pointsArray[i][1] = pointsArray[i][1];
+	inputShape->position[0] = position[0];
+	inputShape->position[1] = position[1];
+	inputShape->sidesCount = sidesCount;
+	inputShape->sides = calloc(1, sizeof(vec2)*sidesCount);
+	for(int i = 0; i<sidesCount; i++){
+		inputShape->sides[i].start[0] = sides[i]->start[0];
+		inputShape->sides[i].start[1] = sides[i]->start[1];
+		inputShape->sides[i].end[0] = sides[i]->end[0];
+		inputShape->sides[i].end[1] = sides[i]->end[1];
 	}
 }
 
@@ -21,12 +24,29 @@ void SetPosShape(shape *inputShape, vec2 desiredPosition){
 
 void SetAngleShape(shape *inputShape, float angle){
 
-	for(int i = 0; i< inputShape->pointCount; i++){
-		glm_vec2_rotate(inputShape->pointsArray[i], glm_rad(angle), inputShape->pointsArray[i]);
+	for(int i = 0; i< inputShape->sidesCount; i++){
+		//glm_vec2_rotate(inputShape->sides[i], glm_rad(angle), inputShape->sides[i]);
 	}
 }
-bool Collides(shape *shape1, shape shape2){
-	// TODO
+bool Collides(shape *shape1, shape* shape2){
+	// Implementing Separating Axes Theorem 
+	// https://dyn4j.org/2010/01/sat/
+	
+	for (int i = 0; i < shape1->sidesCount; i++){
+		// Calculating normal of each side
+		// to use it as an axe
+		vec2 p1 = {0.0f, 0.0f};
+		vec2 p2 = {0.0f, 0.0f};
+		p1[0] = shape1->sides[i].start[0];
+		p1[1] = shape1->sides[i].start[1];
+		p2[0] = shape1->sides[i].end[0];
+		p2[1] = shape1->sides[i].end[1];
+		vec2 edge = {p1[0]-p2[0], p1[1]-p1[1]};
+		vec2 axe = {-edge[1], edge[0]};
+		
+		// Projecting shape on axe
+		double min = glmc_vec2_dot(axe, shape1->sides[i].start);
+
+	}
 	return true;
 }
-
--- source/logic/shape.h
+++ source/logic/shape.h
@@ -4,20 +4,25 @@
 #include <stdlib.h>
 
 // Used for collision check and physics
+typedef struct line
+{
+	vec2 start;
+	vec2 end;
+}line;
 
 typedef struct shape
 {
 	vec2 position;
-	int pointCount;
+	int sidesCount;
 	// All points are relative to position vector
-	vec2 *pointsArray;
+	line *sides;
 }shape;
 
-void InitShape(shape inputShape, vec2 position, float angle, int pointCount, vec2 pointsArray[3]);
+void InitShape(shape* inputShape, vec2 position, float angle, int sidesCount, line* sides[3]);
 
 void SetPosShape(shape* inputShape, vec2 desiredPosition);
 
 void SetAngleShape(shape* inputShape, float angle);
 
-bool Collides(shape* shape1, shape shape2);
+bool Collides(shape* shape1, shape* shape2);
 
